ce66f9b9752d00955cae2f3fdb5eeecdd86fecf1
Patrick Zinner <zinnsoldat91@gmail.com>
2017-10-14 11:04:15 +0200

    root folder into app folder

--- app/src/model/christmasmarketmockdata.ts
+++ app/src/model/christmasmarketmockdata.ts
@@ -33,5 +33,39 @@ export const MARKETS: Christmasmarket[] = [{
       start: 600,
       end: 720
     }]
+},
+{
+  id: 2,
+  name: 'Am Hof oder so',
+  position: { latitude: 48.208912, longitude: 16.373293},
+  start: new Date(2017, 10, 25),
+  end: new Date(2017, 11, 20),
+  address: 'Straße 12',
+  zipcode: '1010',
+  city: 'Wien',
+  rating: 4.3,
+  numberOfRatings: 13,
+  ratingPrice: 3.2,
+  openingHours: [
+    { dayOfWeek: 0, open: true, start: 600, end: 1200 },
+    { dayOfWeek: 1, open: true, start: 600, end: 1200 },
+    { dayOfWeek: 2, open: true, start: 600, end: 1200 },
+    { dayOfWeek: 3, open: true, start: 600, end: 1200 },
+    { dayOfWeek: 4, open: true, start: 600, end: 1200 },
+    { dayOfWeek: 5, open: true, start: 600, end: 1200 },
+    { dayOfWeek: 6, open: true, start: 600, end: 1200 }
+  ],
+  extraordinaryOpeningHours: [{
+    date: new Date(2017, 11, 6),
+    open: false,
+    start: null,
+    end: null
+  },
+    {
+      date: new Date(2017, 11, 7),
+      open: true,
+      start: 600,
+      end: 720
+    }]
 }
 ];
---
+++ app/src/pages/map/map.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { GoogleMaps, GoogleMapOptions, GoogleMap, GoogleMapsEvent } from "@ionic-native/google-maps";
+import { ChristmasMarketService } from "../../services/christmasmarketservice";
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'map.html'
+})
+
+export class MapPage {
+
+  mapElement: HTMLElement;
+  map: GoogleMap;
+
+
+  constructor(
+    private googleMaps: GoogleMaps,
+    private christmasMarketService: ChristmasMarketService
+  ) {
+
+  }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.mapElement = document.getElementById('map');
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: viennaLat,
+          lng: viennaLng
+        },
+        zoom: 10,
+        tilt: 0
+      }
+    };
+
+    this.map = new GoogleMap(this.mapElement, mapOptions);
+    //drop a marker to the market's position
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+          this.christmasMarketService.getMarkets().then(markets => {
+              markets.forEach(market => {
+                  this.map.addMarker({
+                    title: market.name,
+                    icon: 'red',
+                    animation: 'DROP',
+                    position: {
+                      lat: market.position.latitude,
+                      lng: market.position.longitude
+                    }
+                  });
+              })
+          });
+
+      });
+  }
+
+}
+
+const viennaLat: number = 48.208174;
+const viennaLng: number = 16.373819;
